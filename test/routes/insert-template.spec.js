fbfc628ce08c4bf68a1c16f2372005aa29df89a0
Cody Lee <codylee02@gmail.com>
2019-11-26 19:24:09 -0700

    prep for deploy

--- test/routes/insert-template.spec.js
+++ test/routes/insert-template.spec.js
@@ -16,7 +16,7 @@ describe("insert-template endpoints", function() {
   before("make knex instance", () => {
     db = knex({
       client: "pg",
-      connection: process.env.TEST_DB_URL
+      connection: process.env.TEST_DATABASE_URL
     });
     app.set("db", db);
   });
